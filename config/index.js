ffc3ea9ec71757260862c1e8b7fab2e8e002d534
Jasonccj <981015619@qq.com>
2019-02-21 14:32:34 +0800

    ..

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
 'use strict'
 // Template version: 1.2.6
-// see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
 
@@ -12,8 +11,8 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 9528, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
+    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: false,
