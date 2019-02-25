2868dc88d45f34ff5a9ed9d8bc964967a80e31b9
Jasonccj <981015619@qq.com>
2019-02-25 16:34:42 +0800

    #补充上传

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -32,6 +32,7 @@ const user = {
         login(username, userInfo.password).then(response => {
           setToken('admin')
           commit('SET_TOKEN', 'admin')
+          commit('SET_AVATAR', 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif')
           resolve()
         }).catch(error => {
           reject(error)
