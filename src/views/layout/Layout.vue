7f3075a50da7f104673dff274a4ca5684cf34dc8
Jasonccj <981015619@qq.com>
2019-02-25 14:58:14 +0800

    #封装面包屑组件

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="app-wrapper" >
-    LayOut
-    <div class="mobile">
-      mobile
+    <div class="main-container">
+      <navbar/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <script>
+import { Navbar } from './components'
+
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-// @import "src/styles/minxin.scss";
-.app-wrapper {
-  position: relative;
-  height: 100%;
-  width: 100%;
-  background: pink;
-  &.mobile.openSidebar{
-    position: fixed;
-    top: 0
+  @import "src/styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    &.mobile.openSidebar{
+      position: fixed;
+      top: 0;
+    }
   }
-}
-
+  .drawer-bg {
+    background: #000;
+    opacity: 0.3;
+    width: 100%;
+    top: 0;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+  }
 </style>
+
