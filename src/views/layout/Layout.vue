ac5e018b144f03bbb1d32bc3244f3d5483973828
Jasonccj <981015619@qq.com>
2019-02-25 08:45:39 +0800

    #部分提交

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,21 +1,30 @@
 <template>
-  <div>
-    11111
+  <div class="app-wrapper" >
+    LayOut
+    <div class="mobile">
+      mobile
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Layout',
-  methods: {
-    handleClickOutside() {
-      this.$store.dispatch('CloseSideBar', { withoutAnimation: false })
-    }
-  }
+  name: 'Layout'
 }
 </script>
 
-<style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+// @import "src/styles/minxin.scss";
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  background: pink;
+  &.mobile.openSidebar{
+    position: fixed;
+    top: 0
+  }
+}
 
 </style>
