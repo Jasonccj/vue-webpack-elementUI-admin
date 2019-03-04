c965a42a40fd67325fc2f1b9f554dc8b1cdca1d3
Jasonccj <981015619@qq.com>
2019-03-04 13:49:58 +0800

    右侧头像获取问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -53,7 +53,7 @@ const user = {
           //   reject('getInfo: roles must be a non-null array !')
           // }
           // commit('SET_NAME', data.name)
-          // commit('SET_AVATAR', data.avatar)
+          commit('SET_AVATAR', 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif')
           resolve(response)
         }).catch(error => {
           reject(error)
